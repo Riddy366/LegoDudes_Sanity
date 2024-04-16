2e1d4a5fe00f4d4c8af556892a88e5d4802ba1ac
Riddy366 <ridwanabukar851@gmail.com>
2024-04-16 12:13:56 +0200

    Satt opp descriptions for produktene (les mer)

--- frontend/sanity/services/productServices.js
+++ frontend/sanity/services/productServices.js
@@ -5,13 +5,28 @@ export async function fetchAllProducts() {
     const data = await client.fetch(`*[_type == "products"]{
         _id,
         productname,
+        "slug": producturl.current,
         price,
         stock,
         "catname": category->categorytitle,
         "catslug": category->categoryurl.current,
         "image": productimage.asset->url
-    }`)//GROQ spørring, etter {} velger hvilket innhold som sanity skal hente av produktene.
+    }`)
+    return data
+}//GROQ spørring, etter {} velger hvilket innhold som sanity skal hente av produktene.
     // catslug er et object som henter kategoriurl og peker på current slug.
     // asset henter url, vi får ut sanity sin cdn format.
+
+    //Funksjon som henter et produkt basert på en slug:
+export async function fetchProductBySlug(slug) {
+    const data = await client.fetch(`*[_type == "products" && producturl.current == $slug]{
+        productname,
+        description,
+        "categoryname": category->categorytitle,
+        "catslug": category->categoryurl.current,
+        "image": productimage.asset->url,
+        price,
+        stock
+    }`, {slug})
     return data
 }
